9cc02c2a203240eb803e3e2a6b06caac5dfb720c
alextula26 <alex_marc86@mail.ru>
2021-10-18 13:53:24 +0300

    Sidebar

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -5,10 +5,12 @@ import { SidebarProps } from './Sidebar.props';
 import styles from './Sidebar.module.css';
 import { Menu } from '../Menu/Menu';
 
-export const Sidebar = ({ ...props }: SidebarProps): ReactElement => (
-  <>
-    <div {...props}>
-      <Menu />
-    </div>
-  </>
+import Logo from '../logo.svg';
+
+export const Sidebar = ({ className, ...props }: SidebarProps): ReactElement => (
+  <div className={cn(className, styles.sidebar)} {...props}>
+    <Logo className={styles.logo} />
+    <div>поиск</div>
+    <Menu />
+  </div>
 );
